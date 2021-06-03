3b536535d494b2d66779b3bacab4813fcaa2fcf3
@rimma-fe <rimma.fedorova89@gmail.com>
2021-06-03 12:10:48 +0300

    adds changes to 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 document.getElementById("validation-input").onblur = function() {
     console.log(this.value.length);
-    if (this.getAttribute('data-length') === this.value.length) { 
+    if ((parseInt(this.getAttribute('data-length')) === this.value.length)) { 
       this.classList.add('valid');
       
     } else {
-      this.classList.remove('valid');
       this.classList.add('invalid');
+      this.classList.remove('valid');
     }
-  };
+    };
   
 
 
