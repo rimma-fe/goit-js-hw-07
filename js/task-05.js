a130f5efb75cc151641dfe60f52f0fbd98083e52
@rimma-fe <rimma.fedorova89@gmail.com>
2021-06-03 14:07:24 +0300

    adda changes

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,13 @@
+
 const Refs = {
     input: document.querySelector('#name-input'),
     span: document.getElementById('name-output'),
-}
-
-
-Refs.input.addEventListener('input', (event) => {
-    if (event.target.value.length > 0) {
-        Refs.span.textContent = event.target.value;
-    } else if (event.target.value === '') {
+};
+Refs.input.addEventListener('input', () => {
+    let name = Refs.input.value.trim();
+    if (name.length > 0) {
+        Refs.span.textContent = name;
+    } else {
         Refs.span.textContent = 'незнакомец';
     }
 });
